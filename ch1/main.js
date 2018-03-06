7d8e86f0eab855262e7fea03f0b67cc31a300326
dakrink <dakrink@ncsoft.com>
2018-03-06 09:39:20 +0900

    로딩 시 중앙에 원을 그리고 그 안에 hello world를 찍음

--- ch1/main.js
+++ ch1/main.js
@@ -10,6 +10,14 @@ $(document).ready(function() {
   // TODO
   var tool = new Tool();
 
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = 'black';
+  var text = new PointText(200, 200);
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = 20;
+  text.content = 'hello, world';
+  
   tool.onMouseDown = function(event) {
     var c = Shape.Circle(event.point, 20);
     c.fillColor = 'green';
