5f6fdcb65f270c9541b8162d6636c8d6c3a640f2
dakrink <dakrink@ncsoft.com>
2018-03-06 09:18:54 +0900

    컨버스 추가

--- ch1/main.js
+++ ch1/main.js
@@ -3,5 +3,11 @@
 /* 여기 스타일을 씁니다 */
 $(document).ready(function() {
   'use strict';
-  console.log('main.js loaded');
+  // console.log('main.js loaded');
+  paper.install(window);
+  paper.setup(document.getElementById('mainCanvas'));
+
+  // TODO
+
+  paper.view.draw();
 });
